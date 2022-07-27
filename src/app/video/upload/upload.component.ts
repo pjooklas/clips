56188ad74e68cd33d45adbcd7a8bb4f73d4343b5
pjooklas <pjooklas@users.noreply.github.com>
2022-07-27 21:52:35 +0300

    upload form

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
@@ -8,6 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class UploadComponent implements OnInit {
   isDragover = false
   file: File | null = null
+  nextStep = false
+
+  title = new FormControl('', {
+    validators: [
+    Validators.required,
+    Validators.minLength(3)
+    ],
+    nonNullable: true
+  })
+  uploadForm = new FormGroup({
+    title: this.title
+  })
 
   constructor() { }
 
@@ -20,10 +33,17 @@ export class UploadComponent implements OnInit {
     this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
 
     if(!this.file || this.file.type !== 'video/mp4') {
-      
+      return
     }
 
-    console.log(this.file);
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/, '')
+    )
+    this.nextStep = true
+  }
+
+  uploadFile() {
+    console.log('File uploaded');
     
   }
 
